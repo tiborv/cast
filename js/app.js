ea11c5481bcf0669248b1fea9fb9903321f7057f
Tibor Vukovic <tiborvukovic91@gmail.com>
2016-07-26 15:45:20 +0200

    Scripted auto-commit on change (2016-07-26 15:45:20) by gitwatch.sh

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ mediaManager.onLoad = function (event) {
 
 var customMessageBus = castReceiverManager.getCastMessageBus('urn:x-cast:super.awesome.example');
 customMessageBus.onMessage =event => {
-  alertify.log(JSON.stringify(event);
+  alertify.log(JSON.stringify(event));
 );
 
 }
