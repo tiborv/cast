61a9b74799b39649e9cf192e24cda04fc4c01a3d
Tibor Vukovic <tiborvukovic91@gmail.com>
2016-07-26 15:10:16 +0200

    Scripted auto-commit on change (2016-07-26 15:10:16) by gitwatch.sh

--- js/app.js
+++ js/app.js
@@ -2,3 +2,11 @@ window.mediaElement = document.getElementById('media');
 window.mediaManager = new cast.receiver.MediaManager(window.mediaElement);
 window.castReceiverManager = cast.receiver.CastReceiverManager.getInstance();
 window.castReceiverManager.start();
+
+
+window.castReceiverManager.onSenderDisconnected = event => {
+  if(window.castReceiverManager.getSenders().length == 0 &&
+    event.reason == cast.receiver.system.DisconnectReason.REQUESTED_BY_SENDER) {
+      window.close();
+  }
+}
--- static/js/app.bundle.js
+++ static/js/app.bundle.js
@@ -48,6 +48,12 @@
 	window.mediaManager = new cast.receiver.MediaManager(window.mediaElement);
 	window.castReceiverManager = cast.receiver.CastReceiverManager.getInstance();
 	window.castReceiverManager.start();
+	
+	window.castReceiverManager.onSenderDisconnected = event => {
+	  if (window.castReceiverManager.getSenders().length == 0 && event.reason == cast.receiver.system.DisconnectReason.REQUESTED_BY_SENDER) {
+	    window.close();
+	  }
+	};
 
 /***/ }
 /******/ ]);
