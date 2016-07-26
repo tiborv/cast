b3dc18ef2159dbbbb43916e1ad4047dbb388aa6b
Tibor Vukovic <tiborvukovic91@gmail.com>
2016-07-26 16:32:27 +0200

    Scripted auto-commit on change (2016-07-26 16:32:27) by gitwatch.sh

--- static/js/app.bundle.js
+++ static/js/app.bundle.js
@@ -52,64 +52,30 @@
 	
 	function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 	
+	var log = function log(e) {
+	  return alertify.log(JSON.stringify(e));
+	};
+	
 	window.mediaElement = document.getElementById('media');
 	window.mediaManager = new cast.receiver.MediaManager(window.mediaElement);
 	window.castReceiverManager = cast.receiver.CastReceiverManager.getInstance();
 	
 	var appConfig = new cast.receiver.CastReceiverManager.Config();
 	appConfig.statusText = 'TibCast';
-	
 	cast.receiver.logger.setLevelValue(cast.receiver.LoggerLevel.DEBUG);
 	cast.player.api.setLoggerLevel(cast.player.api.LoggerLevel.DEBUG);
 	
 	window.mediaManager['origOnLoad'] = mediaManager.onLoad;
 	mediaManager.onLoad = function (event) {
-	  alertify.log("Loading...");
+	  log("Loading...");
 	  if (window.player !== null) {
-	
 	    player.unload(); // Must unload before starting again.
 	    window.player = null;
 	  }
+	
 	  if (event.data['media'] && event.data['media']['contentId']) {
-	    console.log('Starting media application');
+	    log('Starting media application');
 	    var url = event.data['media']['contentId'];
-	    window.host = new cast.player.api.Host({ 'mediaElement': mediaElement, 'url': url });
-	    var ext = url.substring(url.lastIndexOf('.'), url.length);
-	    var initStart = event.data['media']['currentTime'] || 0;
-	    var autoplay = event.data['autoplay'] || true;
-	    var protocol = null;
-	    mediaElement.autoplay = autoplay; // Make sure autoplay get's set
-	    if (url.lastIndexOf('.m3u8') >= 0) {
-	      // HTTP Live Streaming
-	      protocol = cast.player.api.CreateHlsStreamingProtocol(host);
-	    } else if (url.lastIndexOf('.mpd') >= 0) {
-	      // MPEG-DASH
-	      protocol = cast.player.api.CreateDashStreamingProtocol(host);
-	    } else if (url.indexOf('.ism/') >= 0) {
-	      // Smooth Streaming
-	      protocol = cast.player.api.CreateSmoothStreamingProtocol(host);
-	    }
-	    host.onError = function (errorCode) {
-	      console.log("Fatal Error - " + errorCode);
-	      if (window.player) {
-	        window.player.unload();
-	        window.player = null;
-	      }
-	    };
-	    // If you need cookies, then set withCredentials = true also set any header
-	    // information you need.  If you don't need them, there can be some unexpected
-	    // effects by setting this value.
-	    //      host.updateSegmentRequestInfo = function(requestInfo) {
-	    //        requestInfo.withCredentials = true;
-	    //      };
-	    console.log("we have protocol " + ext);
-	    if (protocol !== null) {
-	      console.log("Starting Media Player Library");
-	      window.player = new cast.player.api.Player(host);
-	      window.player.load(protocol, initStart);
-	    } else {
-	      window.defaultOnLoad(event); // do the default process
-	    }
 	  }
 	  window.mediaManager['origOnLoad'](event);
 	};
